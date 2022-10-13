427aaa4496b9b39d1049c7f129b39ffd094015ed
mastro <gurmeetkhirwar001>
2022-10-13 18:09:14 +0530

    swagger setup

--- routes/ContactenquiryRoute.js
+++ routes/ContactenquiryRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const multer = require("multer");
+// const multer = require("multer");
 const {
    createEnquiry,
   getContactenquiry,
@@ -11,7 +11,7 @@ const {
 
 const router = express.Router();
 
-router.post("/create-enquiry",multer().single('file'), createEnquiry);
+router.post("/create-enquiry", createEnquiry);
 router.get("/get-enquiry", getContactenquiry);
 router.get("/get-single-enquiry/:id", getSingleEnquiry);
 router.put("/update-enquiry/:id", updateenquiry);
--- server.js
+++ server.js
@@ -30,6 +30,7 @@ const StudentclassRoute= require('./routes/studentclassRoute')
 const assignadminclassRoutes = require('./routes/AdminassignclassRoute')
 const adminDocumentRoutes = require('./routes/AdminDocumentRoute')
 const teacheradmin_attendanceRoutes = require('./routes/Teacher_attendanceadminRoute');
+
 const EventadminRoutes=require('./routes/eventadminRoute')
 const studentDailyAttendanceRoutes= require("./routes/studentDailyAttendaceRoute");
 const SubjectRoutes = require("./routes/subjectRoute");
@@ -39,7 +40,8 @@ const documentTeacherRoutes = require('./routes/documentTeacherRoute')
 const eventTeacherRoutes = require('./routes/eventTeacherRoute')
 const studentAttendanceRoutes = require('./routes/studentAttendanceRoute')
 const TeacherattendanceRoutes= require('./routes/TeacherattendanceRoute')
-const TeacherStudentRoutes = require('./routes/Teacher-studentRoute')
+const TeacherStudentRoutes = require('./routes/Teacher-studentRoute');
+const { string } = require("joi");
 
 //const Upload = require('./controllers/uploadFileController')
 const corsOptions = {
@@ -81,6 +83,10 @@ const swaggerOptions = {
       {
         name:"Student",
         description:"Student Route"
+      },
+      {
+        name:"Admin",
+        description: "Admin Route"
       }
     ],
     paths:{
@@ -88,6 +94,39 @@ const swaggerOptions = {
         post:{
           tags:["User"],
           description: "Create new user in system",
+          requestBody:{
+            content: {
+              "application/json" : {
+                schema: {
+                  type:"object",
+                  parameters:[
+                    {
+                      name: "user",
+                      in: "body",
+                      description: "User name",
+                      schema: {
+                        $ref: "#/definitions/User"
+                      }
+                    },
+                  ]
+                }
+              }
+            }
+          },
+          responses: {
+            200: {
+                description: "successful operation",
+                schema: {
+                    "type": "object"
+                }
+            },
+            400: {
+              description: "Invalid status",
+              schema: {
+                  "$ref": "#/definitions/InvalidResponse"
+              }
+          }
+          }
         }
       },
       "/api/user/get-user":{
@@ -294,6 +333,336 @@ const swaggerOptions = {
         delete:{
           tags:["Teacher"],
           description:"Delete individual student attendance created by the teacher"
+        }
+      },
+      "api/semester/create-semester": {
+        post:{
+          tags:["Teacher"],
+          description:"Create new semester in the system "
+        }
+      },
+      "api/student/create-student": {
+        post:{
+          tags:["Student"],
+          description:"Create student in system"
+        }
+      },
+      "api/student/get-student": {
+        get:{
+          tags:["Student"],
+          description:"List of all the students"
+        }
+      },
+      "api/student/get-single-student/:id": {
+        get:{
+          tags:["Student"],
+          description:"getting individual details of the student"
+        }
+      },
+      "api/student/update-student/:id": {
+        put:{
+          tags:["Student"],
+          description:"Update individual details of the student"
+        }
+      },
+      "api/student/delete-student/:id": {
+        delete:{
+          tags:["Student"],
+          description:"Delete individual details of the student"
+        }
+      },
+      "api/dailyAttendance/get-daily-attendance": {
+        get:{
+          tags:["Student"],
+          description:"Getting details of daily attendance"
+        }
+      },
+      "api/dailyAttendance/get-daily-monthly-attendance": {
+        get:{
+          tags:["Student"],
+          description:"Getting details of monthly attendance"
+        }
+      },
+      "api/dailyAttendance/get-semester-attendance": {
+        get:{
+          tags:["Student"],
+          description:"Getting details of semester wise attendance"
+        }
+      },
+      "api/student/assignclass/get-allassign": {
+        get:{
+          tags:["Student"],
+          description:"Getting details of student assign in the class"
+        }
+      },
+      "api/student/assignclass/get-assign/:id": {
+        get:{
+          tags:["Student"],
+          description:"Getting individual details of student assign in the class"
+        }
+      },
+      "api/student/event/get-event": {
+        get:{
+          tags:["Student"],
+          description:"Getting details of student event"
+        }
+      },
+      "api/student/event/get-single-event/:id": {
+        get:{
+          tags:["Student"],
+          description:"Getting details of individual student event"
+        }
+      },
+      "api/student/document/get-document": {
+        get:{
+          tags:["Student"],
+          description:"Getting details of student document"
+        }
+      },
+      "api/student/document/get-single-document/:id": {
+        get:{
+          tags:["Student"],
+          description:"Getting details of individual student document"
+        }
+      },
+      "api/student_class/student_class": {
+        post:{
+          tags:["Student"],
+          description:"Create new student class in the system"
+        }
+      },
+      "api/student_class/get-student_class": {
+        get:{
+          tags:["Student"],
+          description:"Getting details of all student class"
+        }
+      },
+      "api/subject/create-subject": {
+        get:{
+          tags:["Student"],
+          description:"Create list of new subject"
+        }
+      },
+      "api/subject/get-subject": {
+        get:{
+          tags:["Student"],
+          description:"Getting list of all subjects"
+        }
+      },
+      "api/student/teacher_attendance/get-teacher_attendance": {
+        get:{
+          tags:["Student"],
+          description:"Getting list of all teacher attendance"
+        }
+      },
+      "api/teacher_attendance/create-teacher_attendance": {
+        post:{
+          tags:["Admin"],
+          description:"Create teacher attendance in the system"
+        }
+      },
+      "api/teacher_attendance/get-teacher_attendance": {
+        get:{
+          tags:["Admin"],
+          description:"Getting list of teacher attendance"
+        }
+      },
+      "api/teacher_attendance/get -single-teacher_attendance/:id": {
+        get:{
+          tags:["Admin"],
+          description:"Getting details of individual  teacher attendance"
+        }
+      },
+      "api/teacher_attendance/update-teacher_attendance/:id": {
+        put:{
+          tags:["Admin"],
+          description:"Update details of individual teacher attendance"
+        }
+      },
+      "api/teacher_attendance/delete-teacher_attendance/:id": {
+        delete:{
+          tags:["Admin"],
+          description:"Delete details of individual teacher attendance "
+        }
+      },
+      "api/admin/assignclass/create-assign": {
+        post:{
+          tags:["Admin"],
+          description:"Create class assign in the system"
+        }
+      },
+      "api/admin/assignclass/get-assign": {
+        get:{
+          tags:["Admin"],
+          description:"Getting list of class assign "
+        }
+      },
+      "api/admin/assignclass/get-assign/:id": {
+        get:{
+          tags:["Admin"],
+          description:"Getting details of individual class assign"
+        }
+      },
+      "api/admin/assignclass/update-assign/:id": {
+        put:{
+          tags:["Admin"],
+          description:"Update details of individual class assign"
+        }
+      },
+      "api/admin/assignclass/delete-assign/:id": {
+        delete:{
+          tags:["Admin"],
+          description:"Delete details of individual class assign"
+        }
+      },
+      "api/enquiry/create-enquiry": {
+        post:{
+          tags:["Admin"],
+          description:"Create new enquiry in the system"
+        }
+      },
+      "api/enquiry/get-enquiry": {
+        get:{
+          tags:["Admin"],
+          description:"Getting list of all enquiries"
+        }
+      },
+      "api/enquiry/get-single-enquiry/:id": {
+        get:{
+          tags:["Admin"],
+          description:"Getting details of specific enquiry"
+        }
+      },
+      "api/enquiry/update-enquiry/:id": {
+        put:{
+          tags:["Admin"],
+          description:"Update details of enquiry"
+        }
+      },
+      "api/enquiry/delete-enquiry/:id": {
+        delete:{
+          tags:["Admin"],
+          description:"Delete enquiry in the system"
+        }
+      },
+      "api/admin/document/create-document": {
+        post:{
+          tags:["Admin"],
+          description:"Create new document for admin in the system"
+        }
+      },
+      "api/admin/document/get-document": {
+        get:{
+          tags:["Admin"],
+          description:"Getting all the documents of admin"
+        }
+      },
+      "api/admin/document/get-single-document/:id": {
+        get:{
+          tags:["Admin"],
+          description:"Getting individual details of admin"
+        }
+      },
+      "api/admin/document/update-document/:id": {
+        put:{
+          tags:["Admin"],
+          description:"Update details of admin"
+        }
+      },
+      "api/admin/document/delete-document/:id": {
+        delete:{
+          tags:["Admin"],
+          description:"Delete details of admin"
+        }
+      },
+      "api/admin/event/create-event": {
+        post:{
+          tags:["Admin"],
+          description:"Create new event for admin in the system"
+        }
+      },
+      "api/admin/event/get-event": {
+        get:{
+          tags:["Admin"],
+          description:"Getting all the event"
+        }
+      },
+      "api/admin/event/get-single-event/:id": {
+        get:{
+          tags:["Admin"],
+          description:"Getting details of the specific event"
+        }
+      },
+      "api/admin/event/update-event/:id": {
+        put:{
+          tags:["Admin"],
+          description:"Update details of the event"
+        }
+      },
+      "api/admin/event/delete-event/:id": {
+        delete:{
+          tags:["Admin"],
+          description:"Deleting the events"
+        }
+      },
+      "api/schedule/create-schedule": {
+        post:{
+          tags:["Admin"],
+          description:"Create new schedule for admin"
+        }
+      },
+      "api/schedule/get-schedule": {
+        get:{
+          tags:["Admin"],
+          description:"Getting all the details of schedule"
+        }
+      },
+      "api/schedule/get-single-schedule/:id": {
+        get:{
+          tags:["Admin"],
+          description:"Getting details of specific admin schedule"
+        }
+      },
+      "api/schedule/update-schedule/:id": {
+        put:{
+          tags:["Admin"],
+          description:"Updating schedule for admin"
+        }
+      },
+      "api/schedule/delete-schedule/:id": {
+        delete:{
+          tags:["Admin"],
+          description:"Deleting admin schedule"
+        }
+      },
+      "api/teacher-attendance/create-teacher-attendance": {
+        post:{
+          tags:["Admin"],
+          description:"Create new teacher attendance"
+        }
+      },
+      "api/teacher-attendance/get-teacher-attendance": {
+        get:{
+          tags:["Admin"],
+          description:"Getting list of all the teacher attendance"
+        }
+      },
+      "api/teacher-attendance/get-single-teacher-attendance/:id": {
+        get:{
+          tags:["Admin"],
+          description:"Getting details of individual teacher attendance"
+        }
+      },
+      "api/teacher-attendance/update-teacher-attendance/:id": {
+        put:{
+          tags:["Admin"],
+          description:"Update details of teacher attendance"
+        }
+      },
+      "api/teacher-attendance/delete-teacher-attendance/:id": {
+        delete:{
+          tags:["Admin"],
+          description:"Delete details of teacher attendance"
         }
       },
       
